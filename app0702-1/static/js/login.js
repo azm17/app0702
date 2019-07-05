19928c58fada8e4f20f96f2f9f22bfd6de7a83bd
azm <azumi_azumi0917@icloud.com>
2019-07-05 16:37:20 +0900

    aa

--- app0702-1/static/js/login.js
+++
@@ -1,8 +0,0 @@
-function save() {
-    var user = document.getElementById("user");
-    var pass = document.getElementById('pass');
-	//document.cookie = 'user=daiki';
-	//document.cookie = 'pass=miyagawa';
-    sessionStorage.setItem('user', user.value);
-    sessionStorage.setItem('pass', pass.value);
-}
